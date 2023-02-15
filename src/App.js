c06db4d84f27f495a407f77f39be2bc6628ce6a5
piyush <piyushbhele1995@gmail.com>
2023-02-15 23:04:54 +0530

    total

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     this.state = {
       products: [{
         id: 1,
-        price: 99,
+        price: 740,
         title: 'Royal Stag',
         qty: 1,
         img: 'https://file.bungki.com/eyJidWNrZXQiOiJidW5na2kiLCJrZXkiOiJwdWJsaWMv' +
@@ -19,14 +19,14 @@ class App extends React.Component {
       },
       {
         id: 2,
-        price: 999,
+        price: 1360,
         title: 'Blenders Pride',
         qty: 1,
         img: "https://www.delhicapital.com/wp-content/uploads/2022/10/Blenders-Pride-Price-in-Delhi-1024x666.jpg"
       },
       {
         id: 3,
-        price: 9999,
+        price: 2320,
         title: 'Teachers',
         qty: 1,
         img: 'https://cdn3.mydukaan.io/app/image/2000x2000/?url=https://mydukaan-prod-new.s3.amazonaws.com/214913/55ca609f-685b-4b07-a0de-c30a73d64500.png'
@@ -86,6 +86,17 @@ class App extends React.Component {
     return count;
   }
 
+  getCartTotal = () => {
+    const { products } = this.state;
+    let total = 0;
+
+    products.forEach((product) => {
+      total += product.price * product.qty;
+    });
+
+    return total;
+  }
+
 
   render() {
     const { products } = this.state;
@@ -101,9 +112,23 @@ class App extends React.Component {
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
         />
+        <div style={styles.total}>
+          Total : {this.getCartTotal()}
+        </div>
       </div>
     );
   }
 }
+
+const styles = {
+  total: {
+    fontSize: 20,
+    color: '#954e11',
+    fontWeight: 'bold',
+    pading: 10,
+    marginLeft: 20
+  }
+}
+
 
 export default App;
